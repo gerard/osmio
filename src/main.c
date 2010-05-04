0f7fecfec48aab5b6865e33ad657abda4d09e174
Gerard Lledo <gerard.lledo@gmail.com>
2010-05-04 18:30:23 +0300

    Add console code

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 //           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
 
+#include "console.h"
+
 struct multiboot;
 
 int main(struct multiboot *mboot_ptr)
 {
     // All our initialisation calls will go in here.
+    console_clear();
+    console_write("Hello world!\n");
     return 0xDEADBABA;
 }

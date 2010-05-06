2a4c36a2993411d307884ca69facef91a4e91cc1
Gerard Lledo <gerard.lledo@gmail.com>
2010-05-06 17:13:14 +0300

    Add some scancode to char conversions
    At least so we see that it works :)

--- src/keyboard.c
+++ src/keyboard.c
@@ -2,10 +2,31 @@
 #include "isr.h"
 #include "printf.h"
 
+static const char scode_to_char_tbl[256] = {
+    [0x1e] = 'a',
+    [0x1f] = 's',
+    [0x20] = 'd',
+    [0x21] = 'f',
+    [0x22] = 'g',
+    [0x23] = 'h',
+    [0x24] = 'j',
+};
+
+static inline scode_is_break(uint8_t c) { return c & 0x80; }
+
 static void keyboard_callback(registers_t regs)
 {
     uint8_t scode = inb(0x60);
-    printf("Got scancode: %x", scode);
+
+    /* FIXME: Design thingie...
+     * Try to move this to a workqueue or something like that.  It doesn't seem
+     * right to do this in interrupt context.
+     */
+    if (!scode_is_break(scode)) {
+        char c = scode_to_char_tbl[scode];
+        if (c) printf("%c", c);
+        else printf("Unknown scancode: %x\n", scode);
+    }
 
     /* ACK */
     outb(0x20, 0x20);

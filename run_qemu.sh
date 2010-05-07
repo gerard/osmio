62632b421be576e00a9d21e66585629d9351d896
Gerard Lledo <gerard.lledo@gmail.com>
2010-05-07 16:57:11 +0300

    Make ./run_all more user-friendly in non-X environments

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-qemu -fda floppy.img -m 32
+# Don't run if we don't have a X server running
+if [ -n "$DISPLAY" ]
+then
+    qemu -fda floppy.img -m 32
+else
+    # TODO: When the kernel gets support for it, we can run it without X over
+    # the serial port.  Right now, just don't...
+    #qemu -fda floppy.img -m 32 -nographic
+    true
+fi

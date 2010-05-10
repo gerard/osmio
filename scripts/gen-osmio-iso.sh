d326d2e98b96f76098429cceaa706de39fc79fac
Gerard Lledo <gerard.lledo@gmail.com>
2010-05-10 15:09:04 +0300

    Cache stage2 file to avoid multiple downloads

--- scripts/gen-osmio-iso.sh
+++ scripts/gen-osmio-iso.sh
@@ -3,7 +3,6 @@
 set -e
 TMPDIR=`mktemp -d osmio-build.XXXXXX`
 MKISOFS_OPTS='-input-charset utf-8 -R -no-emul-boot -boot-load-size 4 -boot-info-table'
-[ -z "$GRUB_STAGE2" ] && GRUB_STAGE2=/usr/lib/grub/i386-pc/stage2_eltorito
 [ -z "$KERNEL_FILE" ] && KERNEL_FILE=src/kernel
 [ -n "$1" ] && ISO_FILE="$1"
 [ -z "$ISO_FILE" ] && ISO_FILE=osmio.iso
@@ -12,13 +11,11 @@ MENU_LST_FILE=grub/menu.lst
 
 mkdir -p $TMPDIR/boot/grub
 
-if [ -f "$GRUB_STAGE2" ]
-then
-    cp $GRUB_STAGE2 $TMPDIR/boot/grub
-else
-    wget http://dropbox.geeksynapse.net/stage2_eltorito -P $TMPDIR/boot/grub/
-fi
+[   -z "$GRUB_STAGE2" ] && GRUB_STAGE2=/usr/lib/grub/i386-pc/stage2_eltorito
+[ ! -f "$GRUB_STAGE2" ] && GRUB_STAGE2=grub/stage2_eltorito
+[ ! -f "$GRUB_STAGE2" ] && wget http://dropbox.geeksynapse.net/stage2_eltorito -P grub/
 
+cp $GRUB_STAGE2 $TMPDIR/boot/grub
 cp $KERNEL_FILE $TMPDIR
 cp $MENU_LST_FILE $TMPDIR/boot/grub
 
